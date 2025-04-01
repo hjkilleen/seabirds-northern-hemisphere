b1b0358a09b113f9fbc1b82bc55640fd6dc55d1e
Helen Killeen <hkilleen@faralloninstitute.org>
2025-04-01 14:43:03 -0700

    Revise Environmental Trend Plot
    Re-organize environmental trend facets to be by ecosystem rather than by physical driver and season. This improved interpertability as per reviewer comment

--- scripts/figure_environmental_trends.R
+++ scripts/figure_environmental_trends.R
@@ -29,74 +29,38 @@ cols <- c("#14c4ac", "#fc944c", "#8c54fc", "#fc94e4", "#7cdc54", "#fcdc54", "#cc
 #generate simulated data for plotting
 ff.pt <- pltmm(pb.temp.trends, df.pb)
 ff.ps <- pltmm(pb.strat.trends, df.pb)
-ff.bt <- pltmm(b.temp.trends, df.b)
-ff.bs <- pltmm(b.strat.trends, df.b)
+ff.bt <- pltmm(b.temp.trends, filter(df.b, year<2020))#filter out the one Hawaii year at end of time series due to breeding season spanning calendar year change
+ff.bs <- pltmm(b.strat.trends, filter(df.b, year<2020))
 
-#generate subplots
-pt <- ggplot(data = ff.pt) + 
-  geom_ribbon(aes(x = yearno + 1993, ymin = se.lw, ymax = se.hi, fill = PROVINCE), alpha = 0.15) + 
-  geom_line(aes(x = yearno + 1993, y = y, color = PROVINCE), linewidth = 1) + 
-  geom_hline(aes(yintercept = 0), linetype = "dashed", color = "black") + 
-  labs(x = "Year", y = "Scaled Mixed Layer Temperature", title = "Pre-Breeding Season") + 
+#Plot of seasonally stratified temperature and stratification trends by ecosystem
+plot <- ggplot() + 
+  geom_ribbon(data = ff.pt, aes(x = yearno + 1993, ymin = se.lw, ymax = se.hi), fill = "#fc0000", alpha = 0.15) + 
+  geom_line(data = ff.pt, aes(x = yearno + 1993, y = y), linewidth = 1, color = "#fc0000", linetype = "dashed") + 
+  geom_ribbon(data = ff.bt, aes(x = yearno + 1993, ymin = se.lw, ymax = se.hi), fill = "#fc0000", alpha = 0.15) + 
+  geom_line(data = ff.bt, aes(x = yearno + 1993, y = y), linewidth = 1, color = "#fc0000") + 
+  geom_ribbon(data = ff.ps, aes(x = yearno + 1993, ymin = se.lw, ymax = se.hi), fill = "#0000fc", alpha = 0.15) + 
+  geom_line(data = ff.ps, aes(x = yearno + 1993, y = y), linewidth = 1, color = "#0000fc", linetype = "dashed") + 
+  geom_ribbon(data = ff.bs, aes(x = yearno + 1993, ymin = se.lw, ymax = se.hi), fill = "#0000fc", alpha = 0.15) + 
+  geom_line(data = ff.bs, aes(x = yearno + 1993, y = y), linewidth = 1, color = "#0000fc") + 
+  geom_hline(data = ff.bs, aes(yintercept = 0), linetype = "dotted", color = "black") + 
+  labs(x = "Year", y = "Standardized Value") + 
   lims(y = c(-1.5,1.5)) + 
   scale_color_manual(values = cols) + 
   scale_fill_manual(values = cols) + 
-  theme_classic() + 
-  theme(text = element_text(size = 15, family = "Helvetica"),
-        legend.position = "none", plot.margin = unit(c(0,.5,0,.5), "cm"))
+  theme_bw() +
+  theme(
+    axis.text = element_text(size = 10,family="Helvetica"), 
+    axis.title = element_text(size = 10,family="Helvetica"),
+    axis.title.x = element_text(vjust = -2,family="Helvetica",size=12),
+    axis.title.y = element_text(vjust = 2,family="Helvetica",size=12), 
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    legend.position="none")+
+  facet_wrap(~PROVINCE)
 
-ps <- ggplot(data = ff.ps) + 
-  geom_ribbon(aes(x = yearno + 1993, ymin = se.lw, ymax = se.hi, fill = PROVINCE), alpha = 0.15) + 
-  geom_line(aes(x = yearno + 1993, y = y, color = PROVINCE), linewidth = 1) + 
-  geom_hline(aes(yintercept = 0), linetype = "dashed", color = "black") + 
-  labs(x = "Year", y = "Scaled Stratification (PEA)") + 
-  lims(y = c(-1.5,1.5)) + 
-  scale_color_manual(values = cols) + 
-  scale_fill_manual(values = cols) + 
-  theme_classic() + 
-  theme(text = element_text(size = 15, family = "Helvetica"),
-        legend.position = "none", plot.margin = unit(c(0,.5,0,.5), "cm"))
-
-bt <- ggplot(data = ff.bt) + 
-  geom_ribbon(aes(x = yearno + 1993, ymin = se.lw, ymax = se.hi, fill = PROVINCE), alpha = 0.15) + 
-  geom_line(aes(x = yearno + 1993, y = y, color = PROVINCE), linewidth = 1) + 
-  geom_hline(aes(yintercept = 0), linetype = "dashed", color = "black") + 
-  labs(x = "Year", y = "", title = "Breeding Season") + 
-  lims(y = c(-1.5,1.5)) + 
-  scale_color_manual(values = cols) + 
-  scale_fill_manual(values = cols) + 
-  theme_classic() + 
-  theme(text = element_text(size = 15, family = "Helvetica"),
-        legend.position = "none", plot.margin = unit(c(0,.5,0,.5), "cm"))
-
-bs <- ggplot(data = ff.bs) + 
-  geom_ribbon(aes(x = yearno + 1993, ymin = se.lw, ymax = se.hi, fill = PROVINCE), alpha = 0.15) + 
-  geom_line(aes(x = yearno + 1993, y = y, color = PROVINCE), linewidth = 1) + 
-  geom_hline(aes(yintercept = 0), linetype = "dashed", color = "black") + 
-  labs(x = "Year", y = "") + 
-  lims(y = c(-1.5,1.5)) + 
-  scale_color_manual(values = cols) + 
-  scale_fill_manual(values = cols) + 
-  theme_classic() + 
-  theme(text = element_text(size = 15, family = "Helvetica"),
-        legend.position = "none", plot.margin = unit(c(0,.5,0,.5), "cm"))
-
-#plot to extract legend
-leg <- get_legend(ggplot(data = ff.bs) + 
-  geom_ribbon(aes(x = yearno + 1993, ymin = se.lw, ymax = se.hi, fill = PROVINCE), alpha = 0.15) + 
-  geom_line(aes(x = yearno + 1993, y = y, color = PROVINCE), linewidth = 1) + 
-  scale_color_manual(values = cols) + 
-  scale_fill_manual(values = cols) + 
-  theme(text = element_text(size = 15, family = "Helvetica"),
-        legend.position = "bottom", plot.margin = unit(c(0,.5,0,.5), "cm"),
-        legend.title = element_blank()))
-
-#MERGE & SAVE ENVIRONMENTAL TREND PLOT
+#SAVE ENVIRONMENTAL TREND PLOT
 #====
-#plot <- ggarrange(pt, bt, ps, bs, pc, bc, ncol = 2, nrow = 3)
-plot <- ggarrange(pt, bt, ps, bs, ncol = 2, nrow = 2)#combine subplots
-plot_leg <- ggarrange(plot, leg, ncol = 1, nrow = 2, heights = c(4,1))#add legend
-ggsave(filename = "figures/env.trends.jpg", plot = plot_leg, width = 12, height = 9)#save plot
+ggsave(filename = "figures/env.trends.jpg", plot = plot, width = 9, height = 6)#save plot
 #====
 
 #GENERATE SUPPLEMENTAL FIGURES SHOWING TRENDS WITHIN EACH ECOREGION
