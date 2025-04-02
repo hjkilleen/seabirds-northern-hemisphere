3f6ed66892b4e8ff079ed8ebaf5f332f62099cec
Helen Killeen <hkilleen@faralloninstitute.org>
2025-04-02 11:49:16 -0700

    Move prey portfolio code
    Renamed prey portfolio code as figure rather than analysis sccript. Segments of code relevant for generating model results are now in script 4

---
+++ scripts/figure_portfolio_effect.R
@@ -0,0 +1,48 @@
+#Script to generate prey diversity vs trend estimate linear model plot for manuscript. 
+
+#Written by Helen Killeen
+
+# Wed Apr  2 10:27:11 2025 ------------------------------
+
+#SOURCES & DATA
+#====
+source("scripts/0_Seabird_Helpers.R")
+
+res <- readRDS("output/trend_diversity_estimates.rds")#read in model results from 4_GLMM_Biophysical
+#====
+
+#SET UP
+#====
+cols <- c("#14c4ac", "#fc944c", "#8c54fc", "#fc94e4", "#fcdc54")
+driver.labs <- c("Time Series", "Temperature", "Stratification")
+names(driver.labs) <- c("time", "temp", "strat")
+res$driver <- factor(res$driver, levels = c("time", "temp", "strat"))
+#====
+
+#GENERATE PLOT
+#====
+plot <- ggplot() + 
+  geom_hline(data = res[complete.cases(res),], aes(yintercept = 0), linetype = "dotted", color = "black") + 
+  geom_point(data = res[complete.cases(res),], aes(Shannon_Index, trend, color = PROVINCE, shape = season), size = 6) + 
+  geom_smooth(data = res[complete.cases(res),], aes(Shannon_Index, trend), method = "lm", color = "black") +
+  facet_wrap(~driver, labeller = labeller(driver = driver.labs)) + 
+  labs(x = "Shannon-Wiener Index (H')", y = "Trend Estimate", color = "Ecosystem") + 
+  scale_shape_discrete(name = "Model", labels = c("Breeding Season", "Pre-Breeding Season", "Time Series")) + 
+  scale_color_manual(values = cols) + 
+  theme_bw() +
+  theme(
+    axis.text = element_text(size = 10,family="Helvetica"), 
+    axis.title = element_text(size = 10,family="Helvetica"),
+    axis.title.x = element_text(vjust = -2,family="Helvetica",size=12),
+    axis.title.y = element_text(vjust = 2,family="Helvetica",size=12), 
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank())
+  #guides(color = "none")
+
+ggsave(filename = "figures/prey.response.jpg", plot = plot, width = 9, height = 5)#save plot
+  
+#Extract model results for use in manuscript text
+tab_model(lm(trend~Shannon_Index, data = filter(res, driver == "time")), show.ci = .9)#S31.1
+tab_model(lm(trend~Shannon_Index, data = filter(res, driver == "temp")), show.ci = .9)#S31.2
+tab_model(lm(trend~Shannon_Index, data = filter(res, driver == "strat")), show.ci = .9)#S31.3
+#====
